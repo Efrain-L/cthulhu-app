3ccd3534532e120c5dfd5b9b3e1ec28ac4e30bc0
Efrain Lemus <killdragon7257@gmail.com>
2024-12-29 12:13:02 -0800

    made bio elements editable

--- app/details/(tabs)/bio.tsx
+++ app/details/(tabs)/bio.tsx
@@ -1,9 +1,9 @@
 import ThemedSafeAreaView from '@/components/ui/ThemedSafeAreaView';
 import ThemedText from '@/components/ui/ThemedText';
 import React from 'react';
-import { StyleSheet, Image } from 'react-native';
+import { StyleSheet, Image, ScrollView } from 'react-native';
 import { useInvestigator } from '../../../hooks/useInvestigator';
-import ThemedView from '@/components/ui/ThemedView';
+import ThemedTextInput from '@/components/ui/ThemedTextInput';
 
 export default function InvestigatorBio() {
     const investigator = useInvestigator();
@@ -16,41 +16,114 @@ export default function InvestigatorBio() {
         );
     }
 
-    const name = investigator.details.name;
-    const occupation = investigator.details.occupation;
-    const age = investigator.details.age;
-    const pronouns = investigator.details.pronouns;
-    const residence = investigator.details.residence;
-    const birthplace = investigator.details.birthplace;
-    const imagePath = investigator.details.imagePath;
+    const { name, occupation, age, pronouns, residence, birthplace, imagePath } = investigator.details;
+    const {
+        personalDescription,
+        ideologyBeliefs,
+        significantPeople,
+        meaningfulLocations,
+        treasuredPossessions,
+        traits,
+        injuriesScars,
+        phobiasManias,
+        arcaneTomesSpells,
+        encountersWithStrangeEntities
+    } = investigator.backstory;
 
     return (
-        <ThemedView style={styles.container}>
-            <ThemedText>Investigator Bio</ThemedText>
-            <Image source={imagePath} style={styles.image} />
-            <ThemedText>{name}</ThemedText>
-            <ThemedText>{pronouns}</ThemedText>
-            <ThemedText>{occupation}</ThemedText>
-            <ThemedText>{age}</ThemedText>
-            <ThemedText>{residence}</ThemedText>
-            <ThemedText>{birthplace}</ThemedText>
-        </ThemedView>
+        <ThemedSafeAreaView style={styles.container}>
+            <ScrollView contentContainerStyle={styles.scrollContainer}>
+
+                <ThemedText style={styles.heading}>Investigator Bio</ThemedText>
+                <Image source={imagePath} style={styles.image} />
+                
+                <ThemedText style={styles.subHeading}>Name</ThemedText>
+                <ThemedTextInput style={styles.text} multiline numberOfLines={4} value={name} />
+                
+                <ThemedText style={styles.subHeading}>Pronouns</ThemedText>
+                <ThemedTextInput style={styles.text} multiline numberOfLines={4} value={pronouns} />
+                
+                <ThemedText style={styles.subHeading}>Occupation</ThemedText>
+                <ThemedTextInput style={styles.text} multiline numberOfLines={4} value={occupation} />
+                
+                <ThemedText style={styles.subHeading}>Age</ThemedText>
+                <ThemedTextInput style={styles.text} value={`${age}`} numeric/>
+                
+                <ThemedText style={styles.subHeading}>Residence</ThemedText>
+                <ThemedTextInput style={styles.text} multiline numberOfLines={4} value={residence} />
+                
+                <ThemedText style={styles.subHeading}>Birthplace</ThemedText>
+                <ThemedTextInput style={styles.text} multiline numberOfLines={4} value={birthplace} />
+                
+                <ThemedText style={styles.subHeading}>Personal Description</ThemedText>
+                <ThemedTextInput style={styles.text} multiline numberOfLines={4} value={personalDescription} />
+                
+                <ThemedText style={styles.subHeading}>Ideology/Beliefs</ThemedText>
+                <ThemedTextInput style={styles.text} multiline numberOfLines={4} value={ideologyBeliefs} />
+                
+                <ThemedText style={styles.subHeading}>Significant People</ThemedText>
+                <ThemedTextInput style={styles.text} multiline numberOfLines={4} value={significantPeople} />
+                
+                <ThemedText style={styles.subHeading}>Meaningful Locations</ThemedText>
+                <ThemedTextInput style={styles.text} multiline numberOfLines={4} value={meaningfulLocations} />
+                
+                <ThemedText style={styles.subHeading}>Treasured Possessions</ThemedText>
+                <ThemedTextInput style={styles.text} multiline numberOfLines={4} value={treasuredPossessions} />
+                
+                <ThemedText style={styles.subHeading}>Traits</ThemedText>
+                <ThemedTextInput style={styles.text} multiline numberOfLines={4} value={traits} />
+                
+                <ThemedText style={styles.subHeading}>Injuries/Scars</ThemedText>
+                <ThemedTextInput style={styles.text} multiline numberOfLines={4} value={injuriesScars} />
+                
+                <ThemedText style={styles.subHeading}>Phobias/Manias</ThemedText>
+                <ThemedTextInput style={styles.text} multiline numberOfLines={4} value={phobiasManias} />
+                
+                <ThemedText style={styles.subHeading}>Arcane Tomes/Spells</ThemedText>
+                <ThemedTextInput style={styles.text} multiline numberOfLines={4} value={arcaneTomesSpells} />
+                
+                <ThemedText style={styles.subHeading}>Encounters with Strange Entities</ThemedText>
+                <ThemedTextInput style={styles.text} multiline={true} value={encountersWithStrangeEntities} />
+                
+            </ScrollView>
+        </ThemedSafeAreaView>
     );
 };
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+    },
+    scrollContainer: {
         alignItems: 'center',
+        padding: 16,
+    },
+    heading: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginBottom: 16,
+        textAlign: 'center',
+    },
+    subHeading: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        marginTop: 16,
+        textAlign: 'center',
     },
     text: {
-        fontSize: 18,
-        color: '#333',
+        fontSize: 16,
+        marginTop: 8,
+        borderWidth: 1,
+        padding: 8,
+        width: '100%',
+        textAlignVertical: 'top',
+        height: 100,
     },
     image: {
         width: 100,
         height: 100,
         resizeMode: "cover",
         borderRadius: 10,
+        marginBottom: 16,
     },
 });
--- components/ui/ThemedTextInput.tsx
+++ components/ui/ThemedTextInput.tsx
@@ -1,13 +1,58 @@
-import { TextInput, type TextInputProps, StyleSheet, useColorScheme } from 'react-native';
+import { useState } from 'react';
+import { TextInput, TextInputProps, StyleSheet, useColorScheme } from 'react-native';
 
-export default function ThemedTextInput( { style, placeholder, children }: TextInputProps) {
+interface ThemedTextInputProps extends TextInputProps {
+    numeric?: boolean;
+}
+
+export default function ThemedNumericInput({
+    style,
+    placeholder,
+    value,
+    onChangeText,
+    multiline = false,
+    numeric = false,
+    children,
+}: ThemedTextInputProps) {
     const colorScheme = useColorScheme();
     const textStyle = colorScheme === 'dark' ? styles.darkThemeText : styles.lightThemeText;
-    const borderThemeStyle = { borderColor: colorScheme === 'dark' ? "white" : "black" };
-    const placeholderTextColor = colorScheme === 'dark' ? "gray" : "darkgray";
+    const borderThemeStyle = { borderColor: colorScheme === 'dark' ? 'white' : 'black' };
+    const placeholderTextColor = colorScheme === 'dark' ? 'gray' : 'darkgray';
+
+    const [text, setText] = useState(value || '');
+    const [height, setHeight] = useState(40); // default height
+    
+    const handleChangeText = (inputText: string) => {
+        const processedText = numeric ? inputText.replace(/[^0-9]/g, '') : inputText;
+        setText(processedText);
+        
+        if (onChangeText) {
+            onChangeText(processedText);
+        }
+    };
+
+    const handleContentSizeChange = (event: any) => {
+        if (multiline) {
+            setHeight(event.nativeEvent.contentSize.height);
+        }
+    };
 
     return (
-        <TextInput style={[style, textStyle, borderThemeStyle]} placeholder={placeholder} placeholderTextColor={placeholderTextColor}>
+        <TextInput
+            style={[
+                style,
+                textStyle,
+                borderThemeStyle,
+                { height: Math.max(40, height) },
+            ]}
+            placeholder={placeholder}
+            placeholderTextColor={placeholderTextColor}
+            value={text}
+            onChangeText={handleChangeText}
+            multiline={multiline}
+            keyboardType={numeric ? 'numeric' : 'default'}
+            onContentSizeChange={handleContentSizeChange} // adjusting height dynamically for multiline
+        >
             {children}
         </TextInput>
     );
@@ -15,9 +60,9 @@ export default function ThemedTextInput( { style, placeholder, children }: TextI
 
 const styles = StyleSheet.create({
     lightThemeText: {
-        color: "black",
+        color: 'black',
     },
     darkThemeText: {
-        color: "white",
+        color: 'white',
     },
 });
