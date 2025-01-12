3f683a7d17a0faf5b5f4b1c9b1604e42c3325569
Efrain Lemus <killdragon7257@gmail.com>
2025-01-11 19:51:00 -0800

    base quickfire component

--- app/creation/(stats)/quickfire.tsx
+++ app/creation/(stats)/quickfire.tsx
@@ -1,10 +1,36 @@
 import ThemedSafeAreaView from '@/components/ui/ThemedSafeAreaView';
 import ThemedText from '@/components/ui/ThemedText';
+import { StyleSheet, View } from 'react-native';
 
 export default function QuickFireStats() {
     return (
-        <ThemedSafeAreaView>
-            <ThemedText>Quick Fire Component</ThemedText>
+        <ThemedSafeAreaView style={styles.safeView}>
+            <View style={styles.headerContainer}>
+                <ThemedText style={styles.headerText}>Quick Fire Component</ThemedText>
+            </View>
+            <View style={styles.statSelectContainer}>
+                <ThemedText>STR:</ThemedText>
+            </View>
         </ThemedSafeAreaView>
     );
 };
+
+const styles = StyleSheet.create({
+    safeView: {
+        justifyContent: 'flex-start',
+    },
+    headerContainer: {
+        width: '100%',
+        padding: 10,
+    },
+    headerText: {
+        fontSize: 19,
+        fontWeight: 'bold',
+        textAlign: 'center',
+    },
+    statSelectContainer: {
+        flexDirection: 'row',
+        marginHorizontal: 20,
+        borderWidth: 1,
+    }
+});
